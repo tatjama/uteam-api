15c4a4406e1cc7fde5c60f224c32317812379676
Tanja <tanja120a@gmail.com>
2021-12-22 14:06:21 +0100

    Create TypeScript file

--- server.ts
+++ server.ts
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/*', (req, res) => {
-    res.json({"message": 'ok'});
+    res.json({
+        "message": 'ok'
+    });
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log('Server started at port ' + PORT));
