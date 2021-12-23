bb72ed19bcc9792180c1190a44e35e33d9eb577a
Tanja <tanja120a@gmail.com>
2021-12-23 11:41:56 +0100

    Create Readme.md, create all request
    All requests (get, post, delete, put) are replaced with all() request.
    Create README.md with instructions

--- src/controllers/messages.ts
+++ src/controllers/messages.ts
@@ -1,29 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import { message } from '../utility/data';
 
-const getMessages = (req: Request, res: Response, next: NextFunction) => {
-    let  result = message; 
-    return res.status(200).json(result);
-}
-
 const getMessage = (req: Request, res: Response, next: NextFunction) => {
     let result = message;
-    return res.status(200).json(result);
+    return res.status(200).json(result.message);
 }
 
-const updateMessage = (req: Request, res: Response, next: NextFunction) => {
-    let result = message;
-    return res.status(200).json(result);
-}
 
-const deleteMessage = (req: Request, res: Response, next: NextFunction) => {
-    let result = message;
-    return res.status(200).json(result);
-}
-
-const addMessage = (req: Request, res: Response, next: NextFunction) => {
-    let result = message;
-    return res.status(200).json(result);
-}
-
-export default { getMessages, getMessage, updateMessage, deleteMessage, addMessage};
+export default { getMessage };
