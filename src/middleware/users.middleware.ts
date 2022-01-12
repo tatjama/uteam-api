234d2755f20df763e02dce94880483b8829d091a
Tanja <tanja120a@gmail.com>
2022-01-12 05:58:00 +0100

    validation with validator library
    Import validator library,
    create error Handler class MyError

--- src/middleware/users.middleware.ts
+++ src/middleware/users.middleware.ts
@@ -1,44 +1,133 @@
 import { Request, Response, NextFunction } from "express";
 import  UserService  from '../services/user.service';
-import { validationResult } from 'express-validator';
 import { UserDto } from '../dto/user.dto';
-class UsersMiddleware{ 
-     
-    validateBodyFieldsErrors = (req: Request, res: Response, next: NextFunction) => {
-        const errors = validationResult( req);
-        errors.isEmpty()? next(): res.status(400).send({ errors: errors.array() });
-    }
-
+import validator from 'validator';
+import MyError from "../models/messages/MyError";
+class UsersMiddleware{      
+    
      validateUserNoExist = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
         const user: UserDto | null = await UserService.findByEmailOrUsername(req.body.email, req.body.username );
-        user? res.status(400).send({ error: 'User with that username or email already exists' }): next();        
+        user? res.status(400).send( (new MyError( 'find user', 'validation', 400,[{
+                                            message: 'User with that username or email already exists!',
+                                            field: 'username or email'
+                                        }]))): next();        
     }
     
      validatePassword = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
         const user: UserDto | null = await UserService.verifyLogin(req.body.email, req.body.username, req.body.password );        
-        if(user){ 
-          next();           
-        }else{
-            res.status(400).send({ error: 'User with that username or email is not exists' });
-        }
+        user? next(): res.status(403).send(new MyError( 'password', 'validation', 400,[{
+                                                 message: 'Credentials are invalid!',
+                                                field: 'password'
+                                            }]));
     }
 
-    validateLoginUserFieldsExist = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
-        if(req.body && req.body.password){
-            if(  req.body.username ){
-                 req.body.email = '';
-                next();
+    validateRegisterUserFields = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+        req.body.username = validator.trim(req.body.username);
+        req.body.email = validator.trim(req.body.email);
+        req.body.password = validator.trim(req.body.password);
+        const errors: MyError = new MyError('password fields', 'validation' ,400, []);
+        if(!validator.isAlphanumeric(req.body.username, 'sr-RS@latin', {ignore: '%#*-_'})){
+            errors.arrayError.push({
+                message:'User name only excepts letters, numbers or %#*-_',
+                field: 'username',
+            });
+        }
+        if(!validator.isAlpha(req.body.username[0])){
+            errors.arrayError.push({
+                message: 'Username must start with a letter',
+                field: 'username',
+            });
+        }
+        if(!validator.isEmail(req.body.email)){
+            errors.arrayError.push({ 
+                message: 'Invalid email format',
+                field: 'email',
+            });
+        }
+        if(validator.isLength(req.body.password, {min: 0, max:5})){
+            errors.arrayError.push({
+                message: 'Must include password (min 6 characters)',
+                field: 'password',
+             });
+        }
+
+        errors.arrayError.length > 0? res.status(400).send(errors): next();
+                
+    }
+
+    validateLoginUserFields = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+        
+        const errors: MyError = new MyError('login fields', 'validation' ,400, []);
+        if(req.body.username || req.body.email){
+            if(req.body.username){
+                req.body.username = validator.trim(req.body.username);
+                req.body.email = '';
+                if(!validator.isAlphanumeric(req.body.username, 'sr-RS@latin', {ignore: '%#*-_'})){
+                    errors.arrayError.push({
+                        message:'User name only excepts letters, numbers or %#*-_',
+                        field: 'username',
+                    });
+                }
+                if(!validator.isAlpha(req.body.username[0])){
+                    errors.arrayError.push({
+                        message: 'Username must start with a letter',
+                        field: 'username',
+                    });
+                }
+
+            }else{
+                req.body.email = validator.trim(req.body.email);
+                req.body.username = '';
+                if(!validator.isEmail(req.body.email)){
+                    errors.arrayError.push({ 
+                        message: 'Invalid email format',
+                        field: 'email',
+                    });
+                }
+            }  
+            
+        }else{
+            errors.arrayError.push({
+                message: 'Must include username or email',
+                field: 'username or email'           
+            })
+        }
+        req.body.password = validator.trim(req.body.password);
+        if(validator.isLength(req.body.password, {min: 0, max:5})){
+            errors.arrayError.push({
+                message: 'Must include password (min 6 characters)',
+                field: 'password',
+             });
+        }
+
+        errors.arrayError.length > 0? res.status(400).send(errors): next();
+
+        /*if(req.body && req.body.password){
+            if( req.body.username){
+                req.body.username = validator.trim(req.body.username);
+                if(validator.isAlphanumeric(req.body.username, 'sr-RS@latin', {ignore: '%#*-_'}) &&
+                    validator.isAlpha(req.body.username[0])){                    
+                    req.body.email = '';
+                    next();
+                }else{
+                    res.status(400).send({ error: 'Username must start with a letter and only excepts letters, numbers and #%-_*' })
+                }
             }else{
                 if( req.body.email){
-                    req.body.username = '';
-                    next();
+                    req.body.email = validator.trim(req.body.email);
+                    if(validator.isEmail(req.body.email)){
+                        req.body.username = '';
+                        next();
+                    }else{
+                        res.status(400).send({ error: 'Invalid email format'});
+                    }                    
                 }else{
                     res.status(400).send({ error: 'Missing username, email or password' });        
                 }
             }            
         }else{
             res.status(400).send({ error: 'Missing username, email or password' });
-        }
+        }*/
     }
     
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,28 +1,18 @@
 import express from 'express';
 import UsersMiddleware from '../middleware/users.middleware';
 import controller from '../controllers/users.controller';
-import { body } from 'express-validator';
 
 const router: express.Router = express.Router();
 
 router.get('/', controller.getMessage);
 
 router.post('/login', 
-     body('username').trim().not().matches("[^A-Za-z0-9*#%_-]").withMessage('Username only excepts letters, numbers and #%-_*'),     
-     body('username').matches('^[a-zA-Z]').withMessage('Username must start with a letter').optional(), 
-     body('email').trim().isEmail().optional(),
-     body('password').trim().isLength({ min: 6 }).withMessage('Must include password (6+ characters)'),
-     UsersMiddleware.validateBodyFieldsErrors,
-     UsersMiddleware.validateLoginUserFieldsExist,
+     UsersMiddleware.validateLoginUserFields,
      UsersMiddleware.validatePassword, 
      controller.login);
 
 router.post('/register',
-     body('username').trim().not().matches("[^A-Za-z0-9*#%_-]").withMessage('Username only excepts letters, numbers and #%-_*'),     
-     body('username').matches('^[a-zA-Z]').withMessage('Username must start with a letter'),     
-     body('email').trim().isEmail(),
-     body('password').trim().isLength({ min: 6 }).withMessage('Must include password (6+ characters)'),
-     UsersMiddleware.validateBodyFieldsErrors,
+     UsersMiddleware.validateRegisterUserFields,
      UsersMiddleware.validateUserNoExist, 
      controller.register);
 
